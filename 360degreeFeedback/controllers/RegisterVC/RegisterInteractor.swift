e009691e2b915934f2e68439c3c77ec11b6ae3d0
uraankhayayaal <uraankhayayaal@gmail.com>
2019-04-20 12:08:05 +0900

    Created Register

---
+++ 360degreeFeedback/controllers/RegisterVC/RegisterInteractor.swift
@@ -0,0 +1,16 @@
+//
+//  RegisterInteractor.swift
+//  360degreeFeedback
+//
+//  Created by Ayaal Kaplin on 20/04/2019.
+//  Copyright © 2019 Ayaal Kaplin. All rights reserved.
+//
+
+import Foundation
+
+protocol RegisterInteractorProtocol : class {
+    func successRegister()
+    func showError(_ message : String)
+}
+
+
---
+++ 360degreeFeedback/controllers/RegisterVC/RegisterViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RegisterViewController.swift
+//  360degreeFeedback
+//
+//  Created by Ayaal Kaplin on 20/04/2019.
+//  Copyright © 2019 Ayaal Kaplin. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController : UIViewController {
+    
+}
