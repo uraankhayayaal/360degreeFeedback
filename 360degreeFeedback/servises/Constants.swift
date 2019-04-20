3cf83b50c4a1a2d6915fa93882ebb6913e7bc398
gamburger <mail@michil.ru>
2019-04-20 11:44:22 +0900

    Changed constants file

--- 360degreeFeedback/servises/Constants.swift
+++ 360degreeFeedback/servises/Constants.swift
@@ -8,25 +8,6 @@
 
 import Foundation
 
-struct K {
-    struct ProductionServer {
-        static let baseURL = "http://82.202.246.120:8080/"
-    }
-    
-    struct APIParameterKey {
-        static let password = "password"
-        static let email = "email"
-        static let page = "page"
-    }
-}
-
-enum HTTPHeaderField: String {
-    case authentication = "Authorization"
-    case contentType = "Content-Type"
-    case acceptType = "Accept"
-    case acceptEncoding = "Accept-Encoding"
-}
-
-enum ContentType: String {
-    case json = "application/json"
+struct Constants {
+    static let baseURL = "http://82.202.246.120:8080/"
 }
